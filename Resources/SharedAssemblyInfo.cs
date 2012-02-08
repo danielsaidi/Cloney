e3b697cbe8e32ef2d692a0d7ac17c5a1154200e1
Daniel Saidi <daniel.saidi@gmail.com>
2012-02-08 21:21:41 +0100

    Updated version number to 0.9.1.0

--- Resources/SharedAssemblyInfo.cs
+++ Resources/SharedAssemblyInfo.cs
@@ -15,8 +15,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
