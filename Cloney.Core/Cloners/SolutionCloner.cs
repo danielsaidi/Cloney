afb99409eb88389db27c7a3048b0c22941ba5b26
lcorneliussen <me@lcorneliussen.de>
2012-11-02 21:24:10 +0100

    Fix issue #5: using Path.Combine instead of just concatenating the paths

--- Cloney.Core/Cloners/SolutionCloner.cs
+++ Cloney.Core/Cloners/SolutionCloner.cs
@@ -75,7 +75,7 @@ namespace Cloney.Core.Cloners
                     continue;
 
                 var adjustedFilePath = AdjustPath(filePath, sourcePath, sourceNamespace, targetNamespace);
-                var targetFilePath = targetPath + adjustedFilePath;
+                var targetFilePath = Path.Combine(targetPath, adjustedFilePath);
 
                 if (IsPlainCopyFile(fileName))
                 {
@@ -113,7 +113,7 @@ namespace Cloney.Core.Cloners
                     continue;
 
                 var adjustedFolderPath = AdjustPath(directory, sourcePath, sourceNamespace, targetNamespace);
-                var targetFolderPath = targetPath + adjustedFolderPath;
+                var targetFolderPath = Path.Combine(targetPath, adjustedFolderPath);
 
                 if (!Directory.Exists(targetFolderPath))
                     Directory.CreateDirectory(targetFolderPath);
