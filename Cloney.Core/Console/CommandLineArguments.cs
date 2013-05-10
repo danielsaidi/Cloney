626e45bd32553eb0a272baac6a875ff4f07a02d7
unknown <daniel.saidi@DANIELW8C9.cloudnine.internal>
2013-05-10 14:49:46 +0200

    Added new class for cmd line args

---
+++ Cloney.Core/Console/CommandLineArguments.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Cloney.Core.Console
+{
+    public class CommandLineArguments
+    {
+        private readonly IDictionary<string, string> args;
+
+
+        public CommandLineArguments(IDictionary<string, string> args)
+        {
+            this.args = args;
+        }
+
+
+        public IDictionary<string, string> Raw
+        {
+            get { return args; }
+        }
+
+
+        public bool HasArgument(string key)
+        {
+            return (args.ContainsKey(key));
+        }
+
+        public bool HasArgument(string key, string value)
+        {
+            return (args.ContainsKey(key) && args[key] == value);
+        }
+
+        public bool HasSingleArgument(string key)
+        {
+            return args.Count == 1 && HasArgument(key);
+        }
+
+        public bool HasSingleArgument(string key, string value)
+        {
+            return args.Count == 1 && HasArgument(key, value);
+        }
+    }
+}
