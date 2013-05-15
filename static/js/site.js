5afbc4f1d0a29ba4806213581b272d6aaef514e5
Daniel Saidi <daniel.saidi@gmail.com>
2013-05-15 15:36:47 +0200

    Added download

--- static/js/site.js
+++ static/js/site.js
@@ -6,17 +6,16 @@ $(document).ready(function() {
 
 	function updateGitHubInfo() {
 		var api_base = "https://api.github.com/repos/danielsaidi/Cloney";
-		var api_tags = api_base + "/tags?callback=?";
+		var api_tags = api_base + "/tags";
 
-		updateVersionInfo("0.9.3.1");
-
-		/*$.get(api_tags, function(data) {
+		$.get(api_tags, function(data) {
 			updateVersionInfo(data[0]["name"]);
-		});*/
+		});
 	}
 
 	function updateVersionInfo(version) {
 		$(".version-target").html(version);
+		$("a.download-button").attr("href", "downloads/" + version + ".zip");
 	}
 
 	setupExternalLinks();
